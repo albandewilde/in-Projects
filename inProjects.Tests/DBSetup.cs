ebceb47c6368e6b327e94ec3f8148b4190a27751
DCHICHE <dchiche94@hotmail.fr>
2019-04-05 10:33:05 +0200

    Begin DBSetup

--- inProjects.Tests/DBSetup.cs
+++ inProjects.Tests/DBSetup.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using CK.Core;
+using CK.DB.Auth;
+using CK.DB.User.UserPassword;
+using CK.SqlServer;
+using NUnit.Framework;
+using static CK.Testing.DBSetupTestHelper;
+
 
 namespace inProjects.Tests
 {
-    class DBSetup
+    [TestFixture]
+    public class DBSetup : CK.DB.Tests.DBSetup
     {
+        [Test]
+        [Explicit]
+        public void Add_Admin()
+        {
+            var u = TestHelper.StObjMap.StObjs.Obtain<UserPasswordTable>();
+            using( var ctx = new SqlStandardCallContext( TestHelper.Monitor ) )
+            {
+                var result = u.CreateOrUpdatePasswordUser( ctx, 1, 1, "a" );
+                Assert.That( result.OperationResult == UCResult.Created || result.OperationResult == UCResult.Updated );
+            }
+        }
     }
 }
