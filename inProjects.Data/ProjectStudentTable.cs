c1ae2dda214af19eee6dc613f7c2f1c669337d14
Kimor <agopian@intechinfo.fr>
2019-04-12 10:25:28 +0200

    ADD CK.DB.CKtTrait for techno !
    DELETE tTechno + tProjectUseTechno

--- inProjects.Data/ProjectStudentTable.cs
+++ inProjects.Data/ProjectStudentTable.cs
@@ -9,7 +9,7 @@ namespace inProjects.Data
     [Versions( "1.0.0" )]
     public abstract class ProjectStudentTable : SqlTable
     {
-        void StObjConstruct(CK.DB.Actor.GroupTable groupTable, CK.DB.Actor.UserTable userTable)
+        void StObjConstruct( CK.DB.Actor.GroupTable groupTable, CK.DB.Actor.UserTable userTable, CK.DB.SqlCKTrait.CKTraitTable cKTraitTable)
         {
 
         }
