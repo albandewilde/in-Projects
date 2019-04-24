6d553bf7af918cf94e341f3bcce081f8e05f6178
Arthur <acheng@intechinfo.fr>
2019-04-24 13:42:17 +0200

    wip

--- inProjects.SPA/src/views/Home.vue
+++ inProjects.SPA/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="In'Project logo" src="../assets/the_logo-ellipse.png" width="200">
-    <HelloWorld msg="Welcome to In'Project"/>
+
   </div>
 </template>
 
