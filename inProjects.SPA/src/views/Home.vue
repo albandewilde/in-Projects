258ac8313b3550803efb0a2960c9d3102a85a090
Arthur <acheng@intechinfo.fr>
2019-04-24 16:12:54 +0200

    still not working

--- inProjects.SPA/src/views/Home.vue
+++ inProjects.SPA/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img src="../assets/intechLogo.png" width="30%;"><br>
-	next Forum : {{days}}d {{hours}}h {{minutes}}m {{seconds}}s
+	Prochain forum PI dans : {{days}}d {{hours}}h {{minutes}}m {{seconds}}s
   </div>
 </template>
 
@@ -10,13 +10,13 @@ import { Component, Vue } from "vue-property-decorator"
 
 export default class Home extends Vue {
 	nextForum: Date = new Date('July 05, 2019 13:15:00')
-	days: number = 0
-	hours: number = 0
-	minutes: number = 0
-	seconds: number = 0
+	days: number
+	hours: number
+	minutes: number
+	seconds: number
 
 	async mounted() {
-		setInterval(function(){ 
+		setInterval(function() { 
 			var timeToNextForum: number = this.nextForum.getTime() - new Date().getTime()
 		
 			this.days = Math.floor(timeToNextForum / (1000 * 60 * 60 * 24));
