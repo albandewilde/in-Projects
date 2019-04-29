5f7693d9485a0a4cbe35e9a50b5a89bb61bca663
Alban <dewilde@intechinfo.fr>
2019-04-29 11:46:18 +0200

    prepare files for the new toml reading method

---
+++ inProject.TomlHelpers/TomlHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inProject.TomlHelpers
+{
+    struct TomlHelpers
+    {
+        public void GenerateClassFromTomlSchema(string tomlSchema)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetInstanceFromToml<T>(string toml)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
