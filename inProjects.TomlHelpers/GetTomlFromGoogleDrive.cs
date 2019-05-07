384f4b216448a9ff596a96f0e0ded35ba3309445
Alban <dewilde@intechinfo.fr>
2019-05-07 16:20:56 +0200

    create a class for getting the ressource url

---
+++ inProjects.TomlHelpers/GetTomlFromGoogleDrive.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace inProjects.TomlHelpers
+{
+    public static class GetTomlFromGoogleDrive
+    {
+        public static string GetUrlRessource(string url)
+        {
+            string[] splitedUrl = url.Split("?");
+            string path = splitedUrl[0];
+            string query = splitedUrl[1];
+
+            path = path.Replace("open", "uc");
+
+            return path + "?" + query;
+        }
+    }
+}
