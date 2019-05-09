6c7dccc97b677c4eea21befc391106a276a71a1a
Arthur <acheng@intechinfo.fr>
2019-05-09 10:28:32 +0200

    IWebFrontAuthAutoCreateAccountService implementation

--- inProjects.WebApp/Services/AutoCreateAccountService.cs
+++ inProjects.WebApp/Services/AutoCreateAccountService.cs
@@ -1,6 +1,11 @@
 using CK.AspNet.Auth;
+using CK.Auth;
 using CK.Core;
 using CK.DB.Actor;
+using CK.DB.Actor.ActorEMail;
+using CK.DB.Auth;
+using CK.DB.User.UserOidc;
+using CK.SqlServer;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,10 +16,35 @@ namespace inProjects.WebApp.Services
     public class AutoCreateAccountService : IWebFrontAuthAutoCreateAccountService
     {
         readonly UserTable _userTable;
+        readonly ActorEMailTable _actorEmailTable;
+        readonly UserOidcTable _oidcTable;
+        readonly IAuthenticationDatabaseService _dbAuth;
+        readonly IAuthenticationTypeSystem _typeSystem;
 
-        public Task<UserLoginResult> CreateAccountAndLoginAsync( IActivityMonitor monitor, IWebFrontAuthAutoCreateAccountContext context )
+        public AutoCreateAccountService(
+            UserTable userTable,
+            ActorEMailTable actorEmail,
+            UserOidcTable oidcTable,
+            IAuthenticationDatabaseService dbAuth,
+            IAuthenticationTypeSystem typeSystem)
         {
-            throw new NotImplementedException();
+            _userTable = userTable;
+            _actorEmailTable = actorEmail;
+            _oidcTable = oidcTable;
+            _dbAuth = dbAuth;
+            _typeSystem = typeSystem;
+        }
+
+        public async Task<UserLoginResult> CreateAccountAndLoginAsync( IActivityMonitor monitor, IWebFrontAuthAutoCreateAccountContext context )
+        {
+            ISqlCallContext ctx = context.HttpContext.RequestServices.GetService<ISqlCallContext>();
+            int userId = await _userTable.CreateUserAsync( ctx, 1, Guid.NewGuid().ToString() );
+            await _actorEmailTable.AddEMailAsync( ctx, 1, userId, context.HttpContext.User.Identity.Name, true );
+            UCLResult result = await _oidcTable.CreateOrUpdateOidcUserAsync(
+                ctx, 1, userId, (IUserOidcInfo)context.Payload, UCLMode.CreateOrUpdate | UCLMode.WithActualLogin );
+
+            if( result.OperationResult != UCResult.Created ) return null;
+            return await _dbAuth.CreateUserLoginResultFromDatabase( ctx, _typeSystem, result.LoginResult );
         }
     }
 }
