1b841294d0c0e9de7c2ed0f103c27eb5e887b010
Alban <dewilde@intechinfo.fr>
2019-05-13 12:03:17 +0200

    test the interface for register projects

---
+++ Tests/inProject.TomlHelpers.Tests/RegisterProjects.Tests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace inProjects.TomlHelpers.Tests
+{
+    [TestFixture]
+    public class RegisterProjects
+    {
+        [Test]
+        public void the_url_is_invalid()
+        {
+            (bool, string) retour = TomlHelpers.RegisterProject("this isn't a url");
+            Assert.False(retour.Item1);
+        }
+
+        [Test]
+        public void the_url_isnt_a_toml_file()
+        {
+            (bool, string) retour = TomlHelpers.RegisterProject("http://example.com/");
+            Assert.False(retour.Item1);
+        }
+
+        [Test]
+        public void failed_to_parse_the_toml()
+        {
+            (bool, string) retour = TomlHelpers.RegisterProject("https://drive.google.com/open?id=1Jce4M06bOOGLLrufoaMnfhOH30GopdXC");
+            Assert.False(retour.Item1);
+        }
+
+        [Test]
+        public void missing_field_in_the_toml()
+        {
+            (bool, string) retour = TomlHelpers.RegisterProject("https://drive.google.com/open?id=1Jce4M06bOOGLLrufoaMnfhOH30GopdXC");
+            Assert.False(retour.Item1);
+        }
+
+        [Test]
+        public void all_good_the_file_is_valid()
+        {
+            (bool, string) retour = TomlHelpers.RegisterProject("https://drive.google.com/open?id=1DgzQL3-7vmp1xptBH26H543e51eOYDLl");
+            Assert.True(retour.Item1);
+        }
+    }
+}
