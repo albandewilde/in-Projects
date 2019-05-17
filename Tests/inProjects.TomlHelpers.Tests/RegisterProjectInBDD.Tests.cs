06a22981f66dbc43e83d0b54247c518966f56dd2
Alban <dewilde@intechinfo.fr>
2019-05-17 11:50:39 +0200

    prepare tests for register project in BDD

--- Tests/inProjects.TomlHelpers.Tests/RegisterProjectInBDD.Tests.cs
+++ Tests/inProjects.TomlHelpers.Tests/RegisterProjectInBDD.Tests.cs
@@ -1,9 +1,27 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace inProjects.TomlHelpers.Tests
 {
     [TestFixture]
-    class RegisterProjectInBDD
+    public class RegisterProjectInBDD
     {
+        [Test]
+        public static void test_the_SaveProjectPi_method()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public static void test_the_SaveProject_method()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public static void test_the_LinkMembersToProject_method()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
