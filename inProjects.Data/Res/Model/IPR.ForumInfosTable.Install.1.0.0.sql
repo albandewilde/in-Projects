343f4d36b8757aecc56f6727481dfbebc5914acd
Arthur <acheng@intechinfo.fr>
2019-05-22 10:17:41 +0200

    Creates IPR.tForumInfos

---
+++ inProjects.Data/Res/Model/IPR.ForumInfosTable.Install.1.0.0.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IPR.tForumInfos
+(    
+    ProjectId INT NOT NULL,
+    ClassRoom VARCHAR(5),
+    CoordinatesX INT,
+    CoordinatesY INT
+
+    CONSTRAINT PK_tForumInfos_ProjectId PRIMARY KEY (ProjectId),
+    CONSTRAINT FK_tForumInfos_ProjectId FOREIGN KEY (ProjectId) REFERENCES IPR.tProjectStudent(ProjectStudentId)
+)
