b14b2347b3e966f4e8e915f7e262803e6f6f64cf
Alban <dewilde@intechinfo.fr>
2019-05-29 10:04:58 +0200

    Write the class for send e-mail and send Paul an e-mail

---
+++ inProjects.EmailJury/Emailer.cs
@@ -0,0 +1,52 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+
+namespace inProjects.EmailJury
+{
+    public class Emailer
+    {
+        string email;
+        string username;
+        string password;
+        string smtpServerAddress;
+        int port;
+
+        public Emailer(string email, string username, string password, string smtpServerAddress, int port)
+        {
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.smtpServerAddress = smtpServerAddress;
+            this.port = port;
+        }
+
+        public MimeMessage CreateMessage(string to, string subject, string content)
+        {
+            // make the e-mail message
+            MimeMessage message = new MimeMessage();
+            message.To.Add(new MailboxAddress(to));
+            message.From.Add(new MailboxAddress(this.email));
+            message.Subject = subject;
+            message.Body = new TextPart("plain"){Text = content};
+
+            return message;
+        }
+
+        public async Task SendMessage(MimeMessage msg)
+        {
+            using (SmtpClient client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s,c,h,e) => true;
+
+				client.Connect(this.smtpServerAddress, this.port, false);    // Never use Ssl
+				client.Authenticate(this.username, this.password);
+
+				await client.SendAsync(msg);
+
+				client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ inProjects.EmailJury/Program.cs
@@ -0,0 +1,26 @@
+﻿using MimeKit;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace inProjects.EmailJury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonContent = File.ReadAllText("./appsettings.json");
+            JObject identifiant = JObject.Parse(jsonContent);
+
+            Emailer emailer = new Emailer((string)identifiant["gmail"]["address"], (string)identifiant["gmail"]["username"], (string)identifiant["gmail"]["password"], "smtp.gmail.com", 587);
+
+            MimeMessage msg =  emailer.CreateMessage("psimon@intechinfo.fr", "Bonjour Paul", "Voici un e-mail envoyé par l'application In'Projects");
+
+            emailer.SendMessage(msg);
+
+            // Wait the time the async method for sent the email is finished
+            Thread.Sleep(1000);
+        }
+    }
+}
