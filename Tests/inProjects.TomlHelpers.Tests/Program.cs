c1ff3f76f84a29ec16755ac864532646860b0e57
Arthur <acheng@intechinfo.fr>
2019-07-02 15:57:04 +0200

    test

--- Tests/inProjects.TomlHelpers.Tests/Program.cs
+++ Tests/inProjects.TomlHelpers.Tests/Program.cs
@@ -6,10 +6,11 @@ namespace inProjects.TomlHelpers.Tests
 {
     public class Program
     {
-        public static int Main(string[] args)
+        public static int Main( string[] args )
         {
-            var t = System.IO.Path.GetDirectoryName( System.Reflection.Assembly.GetEntryAssembly().Location );
-            return new AutoRun( typeof( Program ).Assembly ).Execute( args, new ExtendedTextWrapper( Console.Out ), Console.In );
+            //var t = System.IO.Path.GetDirectoryName( System.Reflection.Assembly.GetEntryAssembly().Location );
+            //return new AutoRun( typeof( Program ).Assembly ).Execute( args, new ExtendedTextWrapper( Console.Out ), Console.In );
+            return 0;
         }
     }
 }
